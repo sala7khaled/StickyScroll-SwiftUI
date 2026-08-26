8bc8c28fb013f9c182567188054448003fc8368b
sala7khaled <sala7khalad@gmail.com>
2026-08-26 12:39:40 +0300

    Initial Commit

---
+++ StickyScroll/StickyScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyScrollApp.swift
+//  StickyScroll
+//
+//  Created by Salah Khaled on 26/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct StickyScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
